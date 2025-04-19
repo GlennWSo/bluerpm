176d13332cc0591d070ce766d72b305a1d5967fa
gws <glennws@pm.me>
2025-04-19 21:16:22 +0200

    rpm count

--- rpmsensor/src/main.rs
+++ rpmsensor/src/main.rs
@@ -14,48 +14,38 @@ use microbit_bsp::{
 use micromath::F32Ext;
 use {defmt_rtt as _, panic_probe as _};
 
-type SharedFrame = Mutex<ThreadModeRawMutex, Option<Frame<5, 5>>>;
-static FRAME: SharedFrame = SharedFrame::new(None);
-
-#[embassy_executor::task]
-async fn blinker(mut display: LedMatrix, frame: &'static SharedFrame) {
-    loop {
-        let frame = *frame.lock().await;
-        display
-            .display(frame.unwrap(), Duration::from_millis(1))
-            .await;
-    }
-}
-
-#[embassy_executor::task(pool_size = 25)]
-async fn blink(frame: &'static SharedFrame, r: usize, c: usize, ms: u64) {
-    let mut is_on = false;
-    loop {
-        {
-            let mut frame = frame.lock().await;
-            debug!("LED {}:{} is {}", r, c, is_on);
-            if let Some(frame) = frame.as_mut() {
-                if is_on {
-                    frame.set(r, c);
-                } else {
-                    frame.unset(r, c);
-                }
-            }
-        }
-        Timer::after_millis(ms).await;
-        is_on = !is_on;
-    }
-}
+type SharedCounter = Mutex<ThreadModeRawMutex, u32>;
+static COUNTER: SharedCounter = SharedCounter::new(0);
 
 type Btn = Input<'static, AnyPin>;
 #[embassy_executor::task]
-async fn btn_log(mut a: Btn, mut b: Btn) {
+async fn btn_log(mut a: Btn, mut b: Btn, counter: &'static SharedCounter) {
     loop {
         match select(a.wait_for_rising_edge(), b.wait_for_rising_edge()).await {
-            Either::First(_) => println!("a rising"),
+            Either::First(_) => {
+                let mut c = counter.lock().await;
+                *c += 1;
+                // println!("a rising {}", *c);
+            }
             Either::Second(_) => println!("b rising"),
         }
-        Timer::after_millis(10).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn compute_rpm(counter: &'static SharedCounter) {
+    let dt_ms: u64 = 2000;
+    let time_factor = 60.0 * 1000.0 / (dt_ms as f32);
+    let mut damp_rpm: f32 = 0.0;
+    let damp_factor = 0.3;
+    loop {
+        let timer = Timer::after_millis(dt_ms).await;
+        let mut c = (*counter.lock().await) as f32;
+        *counter.lock().await = 0;
+        let latest_rpm = c * time_factor;
+
+        damp_rpm = latest_rpm * damp_factor + (1.0 - damp_factor) * damp_rpm;
+        println!("rpm: {} damp: {}", latest_rpm, damp_rpm);
     }
 }
 
@@ -66,22 +56,8 @@ async fn main(spawner: Spawner) {
 
     let mut display = board.display;
     display.set_brightness(Brightness::MAX);
-    let mut frame = FRAME.lock().await;
-    *frame = Some(Frame::default());
-    spawner.spawn(blinker(display, &FRAME)).unwrap();
-    spawner.spawn(btn_log(board.btn_a, board.btn_b));
-
-    let gold: f32 = 1.618_034;
-    for r in 0..5_i32 {
-        for c in 0..5_i32 {
-            let c_dist = 2 - c;
-            let r_dist = 2 - r;
-            let radi: f32 = ((r_dist.pow(2) + c_dist.pow(2)) as f32).sqrt();
-            let rc_part = (c as f32) * 10. * gold + (r as f32) * 20. * gold;
-            let delay = 100. * gold.powf(radi) + rc_part;
-            spawner
-                .spawn(blink(&FRAME, r as usize, c as usize, delay as u64))
-                .unwrap();
-        }
-    }
+    spawner
+        .spawn(btn_log(board.btn_a, board.btn_b, &COUNTER))
+        .unwrap();
+    spawner.spawn(compute_rpm(&COUNTER)).unwrap();
 }
