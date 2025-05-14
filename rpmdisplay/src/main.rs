d5ce29ca5839db3879dd957258f92ae3d89bb7ed
gws <glennws@pm.me>
2025-05-14 09:58:59 +0200

    display runs ledmatrix

--- rpmdisplay/src/main.rs
+++ rpmdisplay/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_futures::select::{Either, select};
+use embassy_nrf::gpio::{AnyPin, Input, Output};
+use embassy_time::Duration;
+use microbit_bsp::*;
+use nrf_softdevice;
+use {defmt_rtt as _, panic_probe as _};
+
+type Btn = Input<'static, AnyPin>;
+type Led = Output<'static, AnyPin>;
+
+#[embassy_executor::task]
+async fn run_btns(mut a: Btn, mut b: Btn, mut display: display::LedMatrix<Led, 5, 5>) {
+    loop {
+        match select(a.wait_for_low(), b.wait_for_low()).await {
+            Either::First(_) => {
+                defmt::info!("A pressed");
+                display
+                    .display(display::fonts::ARROW_LEFT, Duration::from_secs(1))
+                    .await;
+            }
+            Either::Second(_) => {
+                defmt::info!("B pressed");
+                display
+                    .display(display::fonts::ARROW_RIGHT, Duration::from_secs(1))
+                    .await;
+            }
+        }
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let board = Microbit::default();
+
+    let mut btn_a = board.btn_a;
+    let mut display = board.display;
+    let mut btn_b = board.btn_b;
+
+    display.set_brightness(display::Brightness::MAX);
+    display.scroll("Hello").await;
+    defmt::info!("Application started, press buttons!");
+    spawner.spawn(run_btns(btn_a, btn_b, display));
 }
