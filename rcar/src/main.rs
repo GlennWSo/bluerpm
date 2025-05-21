31c857009d9fe628175a335f4dd2c2a01e1d5b22
gws <glennws@pm.me>
2025-05-21 10:55:54 +0200

    working async with ble

--- rcar/src/main.rs
+++ rcar/src/main.rs
@@ -7,13 +7,22 @@ use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+use nrf_softdevice::ble::advertisement_builder::{
+    AdvertisementDataType, Flag, LegacyAdvertisementBuilder, LegacyAdvertisementPayload,
+};
+use nrf_softdevice::ble::gatt_server::Service;
+use nrf_softdevice::ble::{gatt_server, get_address, peripheral, set_address, Address, Connection};
+use nrf_softdevice::{raw, Softdevice};
+
 #[embassy_executor::main]
 async fn main(s: Spawner) {
     println!("Hello, World!");
     let p = embassy_nrf::init(rcar::config());
     // BLE
     // Spawn the underlying softdevice task
-    info!("wait");
-    Timer::after_millis(100).await;
-    info!("ready");
+    for _ in 0..10 {
+        info!("wait");
+        Timer::after_millis(100).await;
+        info!("ready");
+    }
 }
