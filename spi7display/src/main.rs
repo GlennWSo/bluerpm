1ea17a8a6b0854a04fcb4be6d8b99267580a19cd
gws <glennws@pm.me>
2025-05-28 14:36:34 +0200

    display counter

--- spi7display/src/main.rs
+++ spi7display/src/main.rs
@@ -1,16 +1,17 @@
 #![no_std]
 #![no_main]
 
-use core::ops::ShlAssign;
+use core::{any::Any, ops::ShlAssign};
 
 use defmt::{info, println};
 use embassy_executor::Spawner;
 use embassy_nrf::{
     bind_interrupts,
-    gpio::{AnyPin, Level, Output, OutputDrive, Pin},
+    gpio::{AnyPin, Input, Level, Output, OutputDrive, Pin},
     peripherals::{self, TWISPI0, TWISPI1},
     spim,
 };
+use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
 use embassy_time::Timer;
 use segments::SEGS;
 use {defmt_rtt as _, panic_probe as _};
@@ -28,7 +29,7 @@ use nrf_softdevice::{raw, Softdevice};
 //
 
 mod segments {
-    const S0: u8 = 0b0_000_0000;
+    const S0: u8 = 0b0_011_1111;
     const S1: u8 = 0b0_000_0110;
     const S2: u8 = 0b0_101_1011;
     const S3: u8 = 0b0_100_1111;
@@ -56,23 +57,42 @@ bind_interrupts!(struct Irqs {
     SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1 => spim::InterruptHandler<peripherals::TWISPI1>;
 });
 
+static DISPLAY_NUMBER: Signal<ThreadModeRawMutex, u16> = Signal::new();
+
 #[embassy_executor::task]
 async fn display7(mut spim: spim::Spim<'static, TWISPI1>, mut ncs: Output<'static, AnyPin>) {
     ncs.set_low();
     cortex_m::asm::delay(30);
     spim.write(&[0xFF, 0xFF]).await.unwrap();
     ncs.set_high();
-    let numbers = [1_usize, 3, 3, 7];
-    for (block, num) in numbers.into_iter().rev().enumerate().cycle() {
-        // segments = 1 << j;
-        // segments.overflowing_shl(rhs)
-        //
+    let speed = 1337_u16;
 
-        let mask: u8 = 1 << block;
-        // info!("lit {} at char {:b}", num, mask);
-        ncs.set_low();
-        spim.write(&[SEGS[num], mask]).await.unwrap();
-        ncs.set_high();
+    let mut numbers = [
+        speed / 1000,
+        speed % 1000 / 100,
+        speed % 100 / 10,
+        speed % 10,
+    ]
+    .map(|n| n as usize);
+    loop {
+        if let Some(speed) = DISPLAY_NUMBER.try_take() {
+            info!("new speed: {}", speed);
+            numbers = [
+                speed / 1000,
+                speed % 1000 / 100,
+                speed % 100 / 10,
+                speed % 10,
+            ]
+            .map(|n| n as usize)
+        };
+        for (block, num) in numbers.into_iter().rev().enumerate() {
+            let mask: u8 = 1 << block;
+            // info!("lit {} at char {:b}", num, mask);
+            ncs.set_low();
+            spim.write(&[SEGS[num], mask]).await.unwrap();
+            ncs.set_high();
+            Timer::after_nanos(1000).await;
+        }
     }
 }
 
@@ -92,4 +112,14 @@ async fn main(s: Spawner) {
     let mut ncs = Output::new(p.P0_12.degrade(), Level::High, OutputDrive::Standard);
     Timer::after_millis(100);
     s.spawn(display7(spim, ncs)).unwrap();
+
+    let mut btn_b = Input::new(p.P0_23, embassy_nrf::gpio::Pull::Up);
+    let mut counter = 0_u16;
+    info!("entering buzy loop");
+    loop {
+        btn_b.wait_for_rising_edge().await;
+        info!("btn_b");
+        DISPLAY_NUMBER.signal(counter);
+        counter += 1;
+    }
 }
